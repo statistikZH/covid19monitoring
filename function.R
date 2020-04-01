e8522460ad0ccf8c8d75f763a342721358f40818
Katharina Kaelin <katharina.kaelin@statistik.ji.zh.ch>
2020-04-01 11:29:47 +0200

    rbind 01.04.2020

--- function.R
+++ function.R
@@ -49,7 +49,7 @@ CheckIfDfFollowsStandard <- function (df)  {
 
 CreateMetadata <- function ()  {
   
-covid19monitoring <- read.csv("./covid19socialmonitoring_beta.csv", header=T, sep=",", stringsAsFactors=FALSE, encoding="UTF-8")
+covid19monitoring <- read.csv("./covid19socialmonitoring.csv", header=T, sep=",", stringsAsFactors=FALSE, encoding="UTF-8")
 covid19monitoring_sel <- covid19monitoring[, ! names(covid19monitoring) %in% c("date", "value")]
 unique_rows <- !duplicated(covid19monitoring_sel[names(covid19monitoring_sel)])
 Metadata <- covid19monitoring_sel[unique_rows,]
